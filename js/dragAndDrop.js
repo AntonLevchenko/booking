cc0c4775f07cd6b2d22c25e82c1830e94158cbb5
LevchenkoAnton <levchik03021998@gmail.com>
2019-10-29 22:31:44 +0200

    change drag&drop for map pointer

--- js/dragAndDrop.js
+++ js/dragAndDrop.js
@@ -1,23 +1,39 @@
 (function () {
     let mapPin = document.querySelector('.map__pin--main');
     let mapPins = document.querySelector('.map__pins');
+    let mapFilters = document.querySelector('.map__filters-container');
+    const PIN_SIZES = {
+        width: 62,
+        height: 62
+    };
+    const PIN_TRIANGLE_HEIGHT = 15;
+    const DRAG_LIMIT = {
+        x: {
+            min: PIN_SIZES.width / 2,
+            max: (mapPins.offsetWidth - PIN_SIZES.width / 2)
+        },
+        y: {
+            min: 130,
+            max: mapPins.clientHeight - mapFilters.offsetHeight
+        }
+    };
 
     function limitCoords(moveCoords) {
 
-        if (moveCoords.left < mapPin.offsetWidth / 2) {
-            moveCoords.left = mapPin.offsetWidth / 2;
+        if (moveCoords.left < DRAG_LIMIT.x.min) {
+            moveCoords.left = DRAG_LIMIT.x.min;
         }
 
-        if (moveCoords.left > (mapPins.offsetWidth - mapPin.offsetWidth / 2)) {
-            moveCoords.left = mapPins.offsetWidth - mapPin.offsetWidth / 2;
+        if (moveCoords.left > DRAG_LIMIT.x.max) {
+            moveCoords.left = DRAG_LIMIT.x.max;
         }
 
-        if (moveCoords.top < mapPin.offsetHeight / 2) {
-            moveCoords.top = mapPin.offsetHeight / 2;
+        if (moveCoords.top < DRAG_LIMIT.y.min) {
+            moveCoords.top = DRAG_LIMIT.y.min;
         }
 
-        if (moveCoords.top > mapPins.clientHeight) {
-            moveCoords.top = mapPins.clientHeight;
+        if (moveCoords.top > DRAG_LIMIT.y.max) {
+            moveCoords.top = DRAG_LIMIT.y.max;
         }
     }
 
@@ -25,33 +41,53 @@
         evt.preventDefault();
 
         let dragged = false;
-        let startCoords = {
-            x: evt.clientX,
-            y: evt.clientY
+        // let startCoords = {
+        //     x: evt.clientX,
+        //     y: evt.clientY
+        // };
+
+        let shift = {
+            x: evt.clientX - mapPin.getBoundingClientRect().left,
+            y: evt.clientY - mapPin.getBoundingClientRect().top
         };
 
         function onMouseMove(moveEvt) {
-            dragged = true;
-
-            let shift = {
-                x: startCoords.x - moveEvt.clientX,
-                y: startCoords.y - moveEvt.clientY
-            };
-
-            startCoords = {
-                x: moveEvt.clientX,
-                y: moveEvt.clientY
-            };
 
             let moveCoords = {
-                top: mapPin.offsetTop - shift.y,
-                left: mapPin.offsetLeft - shift.x
+                top: moveEvt.clientY + mapPins.getBoundingClientRect().top + shift.y + PIN_SIZES.height / 2,
+                left: moveEvt.clientX - mapPins.getBoundingClientRect().left + shift.x + PIN_SIZES.width / 2,
             };
-
-            limitCoords(moveCoords);
 
             mapPin.style.top = moveCoords.top + 'px';
             mapPin.style.left = moveCoords.left + 'px';
+            // dragged = true;
+            //
+            // let shift = {
+            //     x: startCoords.x - moveEvt.clientX,
+            //     y: startCoords.y - moveEvt.clientY
+            // };
+            //
+            // startCoords = {
+            //     x: moveEvt.clientX,
+            //     y: moveEvt.clientY
+            // };
+            //
+            // let moveCoords = {
+            //     top: mapPin.offsetTop - shift.y,
+            //     left: mapPin.offsetLeft - shift.x
+            // };
+            //
+            // limitCoords(moveCoords);
+            //
+            // mapPin.style.top = moveCoords.top + 'px';
+            // mapPin.style.left = moveCoords.left + 'px';
+            //
+            // let pointerCoords = {
+            //     x: mapPin.offsetLeft,
+            //     y: mapPin.offsetTop + PIN_TRIANGLE_HEIGHT + PIN_SIZES.height / 2
+            // };
+            //
+            // window.map.setPointerCoords(pointerCoords);
         }
 
         function onMouseUp(upEvt) {
--- js/map.js
+++ js/map.js
@@ -23,6 +23,12 @@ let mapPin = document.querySelector('#map__card')
 let mapPins = map.querySelector('.map__pins');
 let advertisementsList = createAdvertisements();
 let mainPin = map.querySelector('.map__pin--main');
+let addressInput = noticeForm.elements['address'];
+const PIN_SIZES = {
+    width: 46,
+    height: 46
+};
+const PIN_TRIANGLE_HEIGHT = 10;
 
 function generateId() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,c=>(c^crypto.getRandomValues(new Uint8Array(1))[0]&15 >> c/4).toString(16));
@@ -105,8 +111,8 @@ function renderMapPin(card) {
     let pinElement = mapPin.cloneNode(true);
     let pinImg = pinElement.querySelector('img');
 
-    pinElement.style.left = card.location.x - pinElement.offsetWidth / 2 + 'px';
-    pinElement.style.top = card.location.y + pinElement.offsetHeight + 'px';
+    pinElement.style.left = card.location.x - PIN_SIZES.width + 'px';
+    pinElement.style.top = card.location.y + PIN_SIZES.height + PIN_TRIANGLE_HEIGHT + 'px';
     pinElement.setAttribute('data-id', card.id);
 
     pinImg.src = card.author.avatar;
@@ -162,6 +168,14 @@ function enabledForm() {
     renderMapPins();
 }
 
+function setPointerCoords(coords) {
+    addressInput.value = `x: ${coords.x}, y: ${coords.y}`;
+}
+
 mainPin.addEventListener('mouseup', function (e) {
     enabledForm();
 });
+
+window.map = {
+    setPointerCoords
+};
